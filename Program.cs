a331b42ef9b16c51f2f25bd6fc47971d0a882f82
thien <ctthien304@gmail.com>
2024-01-12 21:27:03 +0700

    test

--- Program.cs
+++ Program.cs
@@ -12,21 +12,19 @@ class Program{
     System.Console.WriteLine("enter password");
     string inputPassword = Console.ReadLine();
 
-    if(inputUserName == userName && inputPassword == passWord){
-        System.Console.WriteLine("login success");
-    }else{
-        System.Console.WriteLine("login fail");
-    }
+    System.Console.WriteLine((inputPassword == passWord && inputUserName == userName) ? "login sucess " 
+    : " login fail");
 
     while(true)
     {
-        System.Console.WriteLine("/menu chuc nang");
+        System.Console.WriteLine("menu chuc nang \n");
         System.Console.WriteLine("1. rut tien");
         System.Console.WriteLine("2. nap tien");
         System.Console.WriteLine("3. kiem tra toan khoan ");
         System.Console.WriteLine("4. exit");
 
         System.Console.WriteLine("enter choice");
+
         string choice = Console.ReadLine();
 
         switch(choice){
@@ -51,15 +49,9 @@ class Program{
             System.Console.WriteLine("good bye: ");
             return;
          default:
-            System.Console.WriteLine("lua chon khong hop le ");
+            System.Console.WriteLine("invalid choice: ");
             break;
-        }
-    }
-
-     else
-    {
         System.Console.WriteLine("login fail");
-
     }
     static void RutTien(ref double balance){
         System.Console.WriteLine("nhap so tien muon rut");
@@ -94,7 +86,8 @@ static void NapTien(ref double balance){
 static void KiemTraTaiKhoan (double balance){
     System.Console.WriteLine(" so du hien tai trong tai khoan : " + balance);
 }
-    }
+}
+}
 
 
     
